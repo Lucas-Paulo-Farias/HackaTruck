ee9924bd5fa07d78e020555ecfd2d2f9718906b6
Lucas Paulo de Sousa Farias <159869657+Lucas-Paulo-Farias@users.noreply.github.com>
2025-04-30 15:02:43 -0300

    Update ContentView.swift

--- aa/aa/ContentView.swift
+++ aa/aa/ContentView.swift
@@ -1,7 +1,14 @@
+//
+//  ContentView.swift
+//  desaio1-aula07
+//
+//  Created by Turma01-18 on 28/04/25.
+//
+
 import SwiftUI
 import MapKit
 
-public struct Location: Hashable {
+public struct Location: Hashable{
     var nome: String
     var foto: String
     var descricao: String
@@ -10,33 +17,41 @@ public struct Location: Hashable {
 }
 
 var mapa: [Location] = [
-    Location(nome: "Belo Horizonte", foto: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Bandeira-de-Belo-Horizonte.svg/500px-Bandeira-de-Belo-Horizonte.svg.png", descricao: "Belo Horizonte é um município brasileiro e a capital do estado de Minas Gerais.", latitude: -19.924557, longitude: -43.991597),
-    Location(nome: "Cristo Redentor", foto: "https://lh3.googleusercontent.com/gps-cs-s/AB5caB_2UJ4gzb6xkel4mWw43rzkGkvHHyHxJ7kToldeMzn1udq58_oG5LJ7OkOL9QVpTMSfQlNeQPFbQbd6eH6uKVPDtRXKuAFslUlF-uEVZ2rfU4kQH0_q27SV_OzG67FBSNQpbbNLlw=s1360-w1360-h1020-rw", descricao: "Estátua do Cristo Redentor no Rio de Janeiro", latitude: -22.956, longitude: -43.201),
+    Location(nome: "Belo Horizonte", foto: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Bandeira-de-Belo-Horizonte.svg/500px-Bandeira-de-Belo-Horizonte.svg.png", descricao: "Belo Horizonte é um municipío brasileiro e a capatal do estado de Minas Gerais.", latitude: -19.924557, longitude: -43.991597),
+    Location(nome: "Cristo Redentor", foto: "https://lh3.googleusercontent.com/gps-cs-s/AB5caB_2UJ4gzb6xkel4mWw43rzkGkvHHyHxJ7kToldeMzn1udq58_oG5LJ7OkOL9QVpTMSfQlNeQPFbQbd6eH6uKVPDtRXKuAFslUlF-uEVZ2rfU4kQH0_q27SV_OzG67FBSNQpbbNLlw=s1360-w1360-h1020-rw", descricao: "Estatua do Cristo Redentor no Rio de Janeiro", latitude: -22.956, longitude: -43.201),
 ]
 
 struct SheetView: View {
     var location: Location
-
+    
     var body: some View {
-        VStack {
-            AsyncImage(url: URL(string: location.foto)) { image in
-                image
-                    .image?.resizable()
-                    .frame(width: 250, height: 200)
-            }
-
-            Text(location.nome)
-                .font(.title)
-                .multilineTextAlignment(.center)
-            
-            ZStack {
+        VStack{
+            ZStack{
                 Rectangle()
-                    .foregroundColor(.brown)
-                    .frame(width: 300, height: 500)
-                Text(location.descricao)
-                    .foregroundColor(.white)
-                    .multilineTextAlignment(.center)
-                    .frame(maxWidth: 250, maxHeight: 450)
+                    .foregroundStyle(.amareloPastel)
+                    .ignoresSafeArea()
+                VStack{
+                    Spacer()
+                    AsyncImage(url: URL(string: location.foto)) { image in
+                        image
+                            .image?.resizable()
+                            .frame(width: 250, height: 200, alignment: .center)
+                    }
+                    
+                    Text(location.nome)
+                        .font(.title)
+                        .multilineTextAlignment(.center)
+                    
+                    ZStack{
+                        Rectangle()
+                            .foregroundStyle(Color.brown)
+                            .frame(width: 300, height: 500, alignment: .center)
+                        Text(location.descricao)
+                            .foregroundStyle(Color.white)
+                            .multilineTextAlignment(.center)
+                            .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: 250, minHeight: 0, maxHeight: 450)
+                    }
+                }
             }
         }
     }
@@ -44,38 +59,36 @@ struct SheetView: View {
 
 struct ContentView: View {
     
-    @State private var selectedLocation: Location? = nil
-    @State private var cameraPosition = MapCameraPosition.region(
-        MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: -14.2350, longitude: -51.9253),
-                           span: MKCoordinateSpan(latitudeDelta: 20, longitudeDelta: 20))
+    @State private var mostrarView: Bool = false
+    @State private var position = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: -14.2350, longitude: -51.9253), span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
     )
     
     var body: some View {
-        Map(position: $cameraPosition) {
-            ForEach(mapa, id: \.self) { location in
-                Annotation(location.nome, coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)) {
-                    Button {
-                        selectedLocation = location
-                    } label: {
-                        Image(systemName: "mappin")
-                            .padding()
-                            .background(Color.black)
-                            .foregroundColor(.yellow)
-                            .clipShape(Circle())
+        VStack{
+            Map(){
+                ForEach(mapa, id: \.self) { location in
+                    Annotation(location.nome, coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)){
+                        Button{
+                            mostrarView = true
+                        }label: {
+                            Image(systemName: "mappin")
+                                .font(.title)
+                                .padding()
+                                .background(.black)
+                                .foregroundStyle(Color.yellow)
+                                .clipShape(.capsule)
+                        }
+                        .sheet(isPresented: $mostrarView) {
+                                SheetView(location: location)
+                        }
                     }
                 }
             }
         }
-        .sheet(isPresented: Binding(
-            get: { selectedLocation != nil },
-            set: { if !$0 { selectedLocation = nil } }
-        )) {
-            if let location = selectedLocation {
-                SheetView(location: location)
-            }
         }
     }
-}
 
 #Preview {
     ContentView()
